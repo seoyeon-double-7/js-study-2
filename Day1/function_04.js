d147a9ef010a15d28839801a4d7b1240272a912a
cindy0817-web <s2103@e-mirim.hs.kr>
2022-03-02 19:40:10 +0900

    2단부터 입력한 단까지 곱하는 함수

--- Day1/function_04.js
+++ Day1/function_04.js
@@ -1,3 +1,4 @@
+// 2단부터 입력한 단까지 곱하는 함수
 function gu(ip){
     console.log(`2단부터 ${ip}까지의 곱은`)
     for(let i=2; i<=ip; i++){
@@ -8,4 +9,3 @@ function gu(ip){
 }
 let input = prompt('2단부터 몇단 까지 곱할 것인가요?')
 gu(input)
-// 2단부터 입력한 단까지 곱하는 함수
